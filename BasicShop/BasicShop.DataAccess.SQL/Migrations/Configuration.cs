e4bdc115fda12f4d0df9d5ddb0b6bda6c738f765
Vukasin Stojanovic <vultashin76@gmail.com>
2018-10-07 17:09:50 +0200

    Connected to an SQL Database

---
+++ BasicShop/BasicShop.DataAccess.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace BasicShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BasicShop.DataAccess.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BasicShop.DataAccess.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
