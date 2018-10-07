4b96ebf584299e43073617d5a47d10c7c5c30c93
Vukasin Stojanovic <vultashin76@gmail.com>
2018-10-07 19:21:13 +0200

    Connected to the SQL DB

--- BasicShop/BasicShop.WebUI/App_Start/UnityConfig.cs
+++ BasicShop/BasicShop.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using BasicShop.Core.Contracts;
 using BasicShop.Core.Models;
 using BasicShop.DataAccess.InMemory;
+using BasicShop.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace BasicShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
