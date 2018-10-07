334874d211a5f00295afbabb1d6258c1ca002804
Vukasin Stojanovic <vultashin76@gmail.com>
2018-10-07 13:58:21 +0200

    Hooked up the categories to the products with a dynamic select menu.

---
+++ BasicShop/BasicShop.Core/ViewModels/ProductManagerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BasicShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicShop.Core.ViewModels
+{
+    public class ProductManagerViewModel
+    {
+        public Product Product { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
--- BasicShop/BasicShop.WebUI/Controllers/ProductManagerController.cs
+++ BasicShop/BasicShop.WebUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BasicShop.Core.Models;
+using BasicShop.Core.ViewModels;
 using BasicShop.DataAccess.InMemory;
 
 namespace BasicShop.WebUI.Controllers
@@ -11,10 +12,12 @@ namespace BasicShop.WebUI.Controllers
     public class ProductManagerController : Controller
     {
         ProductRepository context;
+        ProductCategoryRepository productCategories;
 
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
         // GET: ProductManager
         public ActionResult Index()
@@ -25,8 +28,10 @@ namespace BasicShop.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -53,7 +58,10 @@ namespace BasicShop.WebUI.Controllers
                 return HttpNotFound();
             } else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                viewModel.Product = product;
+                viewModel.ProductCategories = productCategories.Collection();
+                return View(viewModel);
             }
         }
 
