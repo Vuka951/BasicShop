dd4ffcc547606dd5c763bcad6babd26264dc466d
Vukasin Stojanovic <vultashin76@gmail.com>
2018-10-07 15:56:32 +0200

    Added a Unity.MVC and configured dependency injecitons for the interface with the container

--- BasicShop/BasicShop.WebUI/Controllers/ProductCategoryManagerController.cs
+++ BasicShop/BasicShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -1,4 +1,5 @@
-﻿using BasicShop.Core.Models;
+﻿using BasicShop.Core.Contracts;
+using BasicShop.Core.Models;
 using BasicShop.DataAccess.InMemory;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,11 @@ namespace BasicShop.WebUI.Controllers
 {
     public class ProductCategoryManagerController : Controller
     {
-        InMemoryRepository<ProductCategory> context;
+        IRepository<ProductCategory> context;
 
-        public ProductCategoryManagerController()
+        public ProductCategoryManagerController(IRepository<ProductCategory> context)
         {
-            context = new InMemoryRepository<ProductCategory>();
+            this.context = context;
         }
         // GET: ProductManager
         public ActionResult Index()
--- BasicShop/BasicShop.WebUI/Controllers/ProductManagerController.cs
+++ BasicShop/BasicShop.WebUI/Controllers/ProductManagerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BasicShop.Core.Contracts;
 using BasicShop.Core.Models;
 using BasicShop.Core.ViewModels;
 using BasicShop.DataAccess.InMemory;
@@ -11,13 +12,13 @@ namespace BasicShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
-        InMemoryRepository<Product> context;
-        InMemoryRepository<ProductCategory> productCategories;
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
 
-        public ProductManagerController()
+        public ProductManagerController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
         {
-            context = new InMemoryRepository<Product>();
-            productCategories = new InMemoryRepository<ProductCategory>();
+            context = productContext;
+            productCategories = productCategoryContext;
         }
         // GET: ProductManager
         public ActionResult Index()
