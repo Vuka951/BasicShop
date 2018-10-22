f8508c0d09956dbcbb3e358f9da3dd6821fb2034
Vukasin Stojanovic <vultashin76@gmail.com>
2018-10-22 19:20:30 +0200

    Tesing for summary view model added

--- BasicShop/BasicShop.WebUI.Tests/Controllers/BasketControllerTest.cs
+++ BasicShop/BasicShop.WebUI.Tests/Controllers/BasketControllerTest.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using BasicShop.Core.Contracts;
+using BasicShop.Core.Models;
+using BasicShop.Core.ViewModels;
+using BasicShop.Services;
+using BasicShop.WebUI.Controllers;
+using BasicShop.WebUI.Tests.Mocks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BasicShop.WebUI.Tests.Controllers
@@ -7,8 +15,59 @@ namespace BasicShop.WebUI.Tests.Controllers
     public class BasketControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanAddBasketItem()
         {
+            //Test Set up
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            var httpContext = new MockHttpContext();
+
+            IBasketService basketService = new BasketServices(products, baskets);
+            var controller = new BasketController(basketService);
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+            //basketService.AddToBasket(httpContext, "1");
+
+            // Test Actions
+            controller.AddToBasket("1");
+
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            // Results
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+        }
+
+        [TestMethod]
+        public void CanGetSummaryViewModel()
+        {
+            //Test Set up
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            products.Insert(new Product() { Id = "1", Price = 10.00m });
+            products.Insert(new Product() { Id = "2", Price = 5.00m });
+
+            Basket basket = new Basket();
+            basket.BasketItems.Add(new BasketItem() { ProductId = "1", Quantity = 2 });
+            basket.BasketItems.Add(new BasketItem() { ProductId = "2", Quantity = 1 });
+            baskets.Insert(basket);
+
+            IBasketService basketService = new BasketServices(products, baskets);
+
+            var controller = new BasketController(basketService);
+            var httpContext = new MockHttpContext();
+
+            //Actions
+            httpContext.Request.Cookies.Add(new System.Web.HttpCookie("eCommerceBasket") { Value = basket.Id });
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+            var result = controller.BasketSummary() as PartialViewResult;
+            var basketSummary = (BasketSummaryViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(3, basketSummary.BasketCount);
+            Assert.AreEqual(25.00m, basketSummary.BasketTotal);
         }
     }
 }
